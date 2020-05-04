8ac48f46652593985bb4bae33b56814312ace793
JUAN M RESTREPO <tinto922@gmail.com>
2020-05-04 19:33:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,8 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
     @article = Article.find(params[:id])
     @article.update(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{@article[:id]}"
   end
 
 end
